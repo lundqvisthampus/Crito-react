b34e94b60e677d586c4af1ebdb9213d3e028e1ed
lundqvisthampus <lundqvisthampus@outlook.com>
2023-11-17 15:10:27 +0100

    final

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import Home from './assets/views/Home';
 import Contact from './assets/views/Contact';
 import News from './assets/views/News';
 import NewsDetails from './assets/views/NewsDetails';
+import NotFound from './assets/views/NotFound';
 import './index.css';
 import ScrollToTop from './assets/components/ScrollToTop';
 import { ArticleProvider } from './assets/contexts/ArticleContext';
@@ -19,6 +20,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path='/contact' element={<Contact />} />
         <Route path='/news' element={<News />} />
         <Route path='/newsdetails/:id' element={<NewsDetails />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
       </ArticleProvider>
     </BrowserRouter>
